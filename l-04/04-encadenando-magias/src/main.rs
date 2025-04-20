c752ccd23bc796e56a4b17ee51059b3275a6b045
Carlos Navajas <c.navajas4@gmail.com>
2025-04-20 21:29:34 +0200

    chore: abrir_cofre fn

--- l-04/04-encadenando-magias/src/main.rs
+++ l-04/04-encadenando-magias/src/main.rs
@@ -2,12 +2,15 @@ use rand::random;
 
 fn abrir_cofre() -> Result<String, String> {
     let unlock: bool = random();
+    if unlock == true {
+        Ok(String::from("Tesoro encontrado!"))
+    } else {
+        Err(String::from("No has podido abrir el cofre"))
+    }
 }
-// 
+
 // fn leer_mensaje(mensaje: String) -> Result<String, String> {
 // }
-
-fn main() {
-    let x: bool = random();
-    println!("@ ----> {}", x);
-}
+// 
+// fn main() {
+// }
