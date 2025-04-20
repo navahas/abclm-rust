57353ac1dd2b31aa173344583b52dff1026bf4f9
Carlos Navajas <c.navajas4@gmail.com>
2025-04-20 22:21:08 +0200

    feat(l-04): complete 04 basic structure

--- l-04/04-encadenando-magias/src/main.rs
+++ l-04/04-encadenando-magias/src/main.rs
@@ -9,8 +9,15 @@ fn abrir_cofre() -> Result<String, String> {
     }
 }
 
-// fn leer_mensaje(mensaje: String) -> Result<String, String> {
-// }
-// 
-// fn main() {
-// }
+fn leer_mensaje(mensaje: String) -> Result<String, String> {
+    if mensaje.is_empty() {
+        Err("no pudo ser".to_string())
+    } else {
+        Ok(mensaje)
+    }
+}
+
+fn main() {
+    let result = abrir_cofre().and_then(leer_mensaje);
+    println!("@ ----> {:?}", result);
+}
