c37343d1b4c8c8a0713d38b37d0adda5171761d8
Carlos Navajas <c.navajas4@gmail.com>
2025-04-21 07:23:26 +0200

    feat(l-04): idiomatic refactor 04

--- l-04/04-encadenando-magias/src/main.rs
+++ l-04/04-encadenando-magias/src/main.rs
@@ -1,23 +1,57 @@
 use rand::random;
+use std::{thread, time};
 
-fn abrir_cofre() -> Result<String, String> {
-    let unlock: bool = random();
-    if unlock == true {
-        Ok(String::from("Tesoro encontrado!"))
-    } else {
-        Err(String::from("No has podido abrir el cofre"))
-    }
+struct Cofre {
+    unlock: bool,
+    mensaje: String
 }
 
-fn leer_mensaje(mensaje: String) -> Result<String, String> {
-    if mensaje.is_empty() {
-        Err("no pudo ser".to_string())
-    } else {
-        Ok("El mensaje es: Sigue aprendiendo Rust".to_string())
+impl Cofre {
+    fn nuevo() -> Self {
+        Cofre {
+            unlock: random(),
+            mensaje: "Sigue aprendiendo Rust".to_string()
+        }
     }
+
+    fn abrir_cofre(&self) -> Result<String, String> {
+        if self.unlock == true {
+            Ok(String::from("Tesoro encontrado!"))
+        } else {
+            Err(String::from("No has podido abrir el cofre"))
+        }
+    }
+
+    fn leer_mensaje(&self) -> Result<String, String> {
+        let msg = format!("El mensaje es: {}", self.mensaje);
+        match self.unlock {
+            true => Ok(msg),
+            false => Err("El cofre esta vacío!".to_string())
+        }
+    }
+
 }
 
 fn main() {
-    let result = abrir_cofre().and_then(leer_mensaje);
-    println!("@ ----> {:?}", result);
+    let mut attempts: u8 = 1;
+    let mut resultado: Result<String, String>;
+
+    loop {
+        let cofre = Cofre::nuevo();
+        resultado = cofre
+            .abrir_cofre()
+            .and_then(|_| cofre.leer_mensaje());
+
+        println!("");
+        println!("{:?}", resultado);
+        if resultado.is_ok() || attempts >= 3 {
+            break;
+        }
+
+        println!("Intentando abrir el cofre de nuevo..... {}/3 intentos", attempts);
+        attempts += 1;
+
+        thread::sleep(time::Duration::from_secs(2))
+    }
+
 }
